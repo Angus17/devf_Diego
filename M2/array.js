9c1a73ed190922e248db100c6304b3f7b4a760cf
Diego <dileo312@hotmail.comclear>
2024-02-02 23:32:10 -0600

    Modulo 1 y 2

---
+++ M2/array.js
@@ -0,0 +1,2 @@
+let list = []
+let another_list = []
---
+++ M2/operadores.js
@@ -0,0 +1,57 @@
+/* Operadores de incremento y decremento */
+
+let iterador = 0;
+
+console.log(iterador);
+
+iterador++;
+
+console.log(iterador);
+iterador--;
+console.log(iterador)
+iterador += 2;
+console.log(iterador)
+iterador += 2;
+console.log(iterador)
+
+/* Operadores de comparacion */
+
+console.log(2 > 9);
+console.log(2 < 9);
+console.log(4 <= 4);
+console.log(4 > 4);
+console.log(3 != 4);
+
+// == ===
+
+let variable_1 = 5;
+let variable_2 = 10;
+let variable_3 = '5';
+
+/* console.log();
+console.log();
+console.log(); */
+
+// Operadores logicos
+
+console.log(true && false)
+console.log(true || false)
+
+/* Si se declara una variable y la asignamos, es true por defecto */
+
+let value_1 = Number('5')
+let value_2 =  9
+let suma = value_1 + value_2
+console.log(suma)
+
+// CONDICIONALES
+
+if (1) 
+    {
+        console.log("Valor verdadero")
+    }
+    else
+        {
+            console.log("Valor falso")
+        }
+    
